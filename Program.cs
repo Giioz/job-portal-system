8ec76b61d6d4741e309766dc9013c1c2796e50d4
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-11-11 23:22:19 +0400

    wip: setting up console UI

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JobPortalSystem;
+using JobPortalSystem.Models;
+
+JobPortalApp jobPortal = new JobPortalApp();
+
+jobPortal.CreateNewJob();
+jobPortal.DisplayJobs();
