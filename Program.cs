917f38cf28ae5820d606ccdb9af83d71b73fd055
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-11-13 22:14:08 +0400

    feat: added admin functions

--- Program.cs
+++ Program.cs
@@ -22,11 +22,20 @@ while(run)
                 auth = true;
                 while(auth)
                 {
-                    if(authenticatedUser.Role == "Seeker")
-                        JobSeekerOptions(authenticatedUser);
+                    if(authenticatedUser.BanStatus == false)
+                    {
+                        if(authenticatedUser.Role == "Seeker")
+                            JobSeekerOptions(authenticatedUser);
 
-                    else if(authenticatedUser.Role == "Employer")
-                        EmployerOptions(authenticatedUser);
+                        else if(authenticatedUser.Role == "Employer")
+                            EmployerOptions();
+                        else if(authenticatedUser.Role == "Admin")
+                            AdminOptions();
+                    }else
+                    {
+                        System.Console.WriteLine("You Have Been Banned.");
+                        break;
+                    }
                 }
             }
             break;
@@ -62,7 +71,7 @@ void JobSeekerOptions(User authenticatedUser)
             break;
     }
 }
-void EmployerOptions(User authenticatedUser)
+void EmployerOptions()
 {
     jobPortal.ShowEmployerMenu();
     var opt = Console.ReadLine();
@@ -80,4 +89,40 @@ void EmployerOptions(User authenticatedUser)
             break;
     }
 
+}
+void AdminOptions()
+{   
+    jobPortal.ShowAdminMenu();
+    var opt = Console.ReadLine();
+
+    switch (opt)
+    {
+        case "1":
+            jobPortal.DisplayUsers();
+            System.Console.WriteLine("Choose User with ID :");
+            int userId = int.Parse(Console.ReadLine());
+            jobPortal.AdminUserOptions(); 
+            var cmd = Console.ReadLine();
+
+            if(cmd == "1")
+                jobPortal.DeleteUser(userId);
+            else if(cmd == "2")
+                jobPortal.BanUser(userId);
+            else if(cmd == "3")
+            {
+                System.Console.WriteLine("Role : 1. Job Seeker, 2. Employer, 3. Admin");
+                var roleCode = Console.ReadLine();
+                string role = (roleCode == "1") ? "Seeker" : (roleCode == "2") ? "Employer" : (roleCode == "3") ? "Admin" : "undefined";
+                jobPortal.ChangeUserRole(userId, role);
+            }
+            else if(cmd == "4")
+                break;
+            break;
+            
+        case "2":
+            jobPortal.DisplayJobs();
+            break;
+        case "3":
+            break;
+    }
 }
